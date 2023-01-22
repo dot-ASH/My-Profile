d3c79a5fecbc4772d07b6bfd285921c513ba1491
Nafees Ashker Sakir <sakir.ashker@gmail.com>
2023-01-22 13:21:06 +0600

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "../styles/globals.css";
 import "../Assests/css/main.scss";
+import "../Assests/js/main.js";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
